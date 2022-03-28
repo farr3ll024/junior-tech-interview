349e22bf55bb6c0c620154b796071fc3521285db
Caitlin Wood <caitlin.m.wood@icloud.com>
2022-03-28 13:15:58 -0400

    add base html page and functions to fetch data

---
+++ index.js
@@ -0,0 +1,9 @@
+const fetchPatients = async () => {
+    const response = await axios.get('https://ti-patient-service.azurewebsites.net/patients');
+    return response?.data;
+};
+
+const fetchPatient = async (patientId) => {
+    const response = await axios.get(`https://ti-patient-service.azurewebsites.net/patients/${patientId}`);
+    return response?.data;
+};
